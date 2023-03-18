2869a916b850669610836375252ff1a7b2ed609a
Punit Arani <punitsai36@gmail.com>
2023-03-18 12:58:55 -0700

    Format with prettier

--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,35 +1,24 @@
-// create nav bar component with 3 options: home, existing pools, create pools
-import React from 'react';
-import Link from 'next/link';
-// import ConnectWalletButton from './ConnectWalletButton.js';
+import React from "react";
+import Link from "next/link";
 
-
-export default function Navbar({}) {
-    
-    return (
-        <div className='flex justify-between h-12 bg-white text-gray-800 w-full p-2'>
-        <div className='flex items-center'>
-            <Link href='/' className='font-bold text-xl mx-2'>PoolMyNFT</Link>
-            <Link href='/'>
-            <div className='ml-4'>Home</div>
-            </Link>
-            <Link href='/existingpools'>
-            <div className='ml-4'>Existing Pools</div>
-            </Link>
-            <Link href='/createpool'>
-            <div className='ml-4'>Create Pool</div>
-            </Link>
-            <Link href='/contracts'>
-            <div className='ml-4'>Contracts</div>
-            </Link>
-        </div>
-        <div className='flex items-center'>
-            {/* <ConnectWalletButton connectedAddress={connectedAddress} setAddress={setAddress}/> */}
-        </div>
-        </div>
-    )
+export default function Navbar() {
+  return (
+    <div>
+      <div>
+        <Link href="/">LiquidPool</Link>
+        <Link href="/">
+          <div>Home</div>
+        </Link>
+        <Link href="/existingpools">
+          <div>Existing Pools</div>
+        </Link>
+        <Link href="/createpool">
+          <div>Create Pool</div>
+        </Link>
+        <Link href="/contracts">
+          <div>Contracts</div>
+        </Link>
+      </div>
+    </div>
+  );
 }
-
-
-
-
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import NavBar from "./components/NavBar";
 import styles from "./page.module.css";
@@ -8,12 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.title}>LiquidPool</h1>
       <NavBar />
       <div className={styles.description}></div>
-
       <div className={styles.center}></div>
-
       <div className={styles.grid}></div>
     </main>
   );
