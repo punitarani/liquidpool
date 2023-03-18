7f56e13bfbb7e19880225bb214c9bc652e59d195
Punit Arani <punitsai36@gmail.com>
2023-03-18 16:10:39 -0700

    Add ConnectWalletButton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,9 @@
 	import ConnectWalletButton from './home/ConnectWalletButton.svelte';
 </script>
 
-<div>
-	<h1>LiquidPool</h1>
-	<ConnectWalletButton />
+<div class="w-full flex align-middle justify-center">
+	<div class="w-1/2 flex flex-row justify-between">
+		<h1>LiquidPool</h1>
+		<ConnectWalletButton />
+	</div>
 </div>
---
+++ src/routes/home/ConnectWalletButton.svelte
@@ -0,0 +1,14 @@
+<script>
+	let user_account = null;
+
+	// Connect MetaMask Wallet
+	async function connect_wallet_handler() {}
+</script>
+
+{#if user_account}
+	<button type="button" class="btn btn-base variant-filled">{user_account}</button>
+{:else}
+	<button type="button" class="btn btn-base variant-filled" on:click={connect_wallet_handler}
+		>Connect MetaMask Wallet</button
+	>
+{/if}
