d38b2540ec04055264cbd471e5815e7e4ff5d887
Punit Arani <punitsai36@gmail.com>
2023-03-18 19:03:21 -0700

    Fix css files

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,7 @@
 	import '../theme.postcss';
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	import '@skeletonlabs/skeleton/styles/all.css';
-	import '../app.postcss';
+	import '../app.css';
 </script>
 
 <slot />
