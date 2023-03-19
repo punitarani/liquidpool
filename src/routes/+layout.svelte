6b5092bef2e0848d98167676fc3215448a26bc20
Jon Sundin <sundinjon@gmail.com>
2023-03-18 17:23:27 -0700

    added theme.postcss to layout - removed css imports from page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script>
-	import '../app.css';
-
 	import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
 	import '@skeletonlabs/skeleton/styles/all.css';
+	import '../theme.postcss';
+	import '../app.css';
 </script>
 
 <slot />
